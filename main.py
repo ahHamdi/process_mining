e25605e4fc68e23cb963d6b64d278e0c6191d7d2
ahamdi <ahamdi@ADERA07.univ-lr.fr>
2025-01-31 16:43:54 +0100

    main added

---
+++ main.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+#Python  libraries
+import numpy as np
+import pandas as pd
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_columns', None)
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+#load dataset
+hiring_data = pd.read_excel('dataset.xlsx')
+hiring_data = pd.read_excel('dataset.xlsx')
+hiring_data = pd.read_excel('dataset.xlsx')
+hiring_data = pd.read_excel('dataset.xlsx')
+data.head()
+
+#data exploration
+#data preprocessing
+
+#modeling
+
+#evaluation
+
+#PCA
+#anomaly detection
+#dclustering
